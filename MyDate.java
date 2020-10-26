e435c37c301c3a1b8b1eab2d197d3d6b9788159e
mahesh.karad <mahesh.karad@apisero.com>
2020-10-26 10:37:41 +0530

    this is first commit v1.0

---
+++ MyDate.java
@@ -0,0 +1,19 @@
+public class MyDate  
+{
+  private int day,month,year;
+
+  MyDate()
+  {
+   day=20;month=10;year=2020;
+  }
+  int getDay(){ return day;}
+  void setDay(int d){ day=d;}  
+  void display(){System.out.println(day+"/"+month+"/"+year);}
+  static void show(){System.out.println("in static method");}
+  public static void main(String args[])
+  {
+   MyDate d= new MyDate();
+   d.display();
+   MyDate.show();
+  }
+}
